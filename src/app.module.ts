91c97f86eb0eccec54a5f2b29bbcbf6b188d711f
11331133 <81962996+11331133@users.noreply.github.com>
2022-02-05 12:57:48 +0900

    Generate project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PostModule, UserModule, CommentModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
