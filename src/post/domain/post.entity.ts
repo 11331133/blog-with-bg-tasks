4d266344e501a5166e7e43cb0473d134010ec214
11331133 <81962996+11331133@users.noreply.github.com>
2022-02-05 17:07:34 +0900

    Add Post entity

---
+++ src/post/domain/post.entity.ts
@@ -0,0 +1,24 @@
+export class Post {
+  constructor(
+    private _title: string,
+    private _body: string,
+    private _author: object,
+    private _publishedAt: Date,
+  ) {}
+
+  public get title(): string {
+    return this._title;
+  }
+
+  public get body(): string {
+    return this._body;
+  }
+
+  public get author(): object {
+    return this._author;
+  }
+
+  public get publishedAt(): Date {
+    return this._publishedAt;
+  }
+}
