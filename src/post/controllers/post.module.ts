9f103187b0e85224c9f10aca4049549fb69fae0d
11331133 <81962996+11331133@users.noreply.github.com>
2022-02-06 17:24:38 +0900

    Add Dockerfile, Docker compose settings

--- src/post/controllers/post.module.ts
+++ src/post/controllers/post.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PostService } from './post.service';
 import { PostResolver } from './post.resolver';
 
 @Module({
-  providers: [PostResolver, PostService],
+  providers: [PostResolver],
 })
 export class PostModule {}
