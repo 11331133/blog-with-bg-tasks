2bb5f6fe0826b7631280d57488d6ac239893211a
11331133 <81962996+11331133@users.noreply.github.com>
2022-02-06 13:15:25 +0900

    Basic interface of Post Resolver

--- src/post/controllers/post.resolver.ts
+++ src/post/controllers/post.resolver.ts
@@ -1,4 +1,38 @@
+import {
+  Args,
+  Int,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import { createPostDTO, updatePostDTO } from '../domain/post.dto';
+import { PostService } from '../domain/post.service';
+import { PostGraphQLModel } from './post.graphql-model';
 
+@Resolver(() => PostGraphQLModel)
 export class PostResolver {
+  constructor(private postService: PostService) {}
 
+  @ResolveField(() => User)
+  async author(@Parent() post: PostGraphQLModel) {}
+
+  @ResolveField(() => [Comment])
+  async comments(@Parent() post: Post) {}
+
+  @Query(() => PostGraphQLModel, { name: 'post' })
+  async getPost(@Args('id', { type: () => Int }) id: number) {}
+
+  @Query(() => Object)
+  async getPaginatedPosts(@Args('paginationInfo') paginationInfo: object) {}
+
+  @Mutation(() => PostGraphQLModel)
+  async createPost(@Args('createPostDTO') createPostDTO: createPostDTO) {}
+
+  @Mutation(() => PostGraphQLModel)
+  async updatePost(@Args('updatePostDTO') updatePostDTO: updatePostDTO) {}
+
+  @Mutation(() => {})
+  async deletePost(@Args('deletePostDTO') deletePostDTO: deletePostDTO) {}
 }
