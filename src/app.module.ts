98a3c960cafce72233418b5c830750ed9552b053
11331133 <81962996+11331133@users.noreply.github.com>
2022-02-10 21:38:18 +0900

    Remove hard coded Sequelize configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+import { Dialect } from 'sequelize/dist';
 
 @Module({
   imports: [
@@ -20,16 +22,18 @@ import { ConfigModule } from '@nestjs/config';
       autoSchemaFile: join(process.cwd(), 'src/auto-generated-schema.gql'),
       sortSchema: true,
     }),
-    SequelizeModule.forRoot({
-      autoLoadModels: true,
-      database: 'blog-with-bg-tasks',
-      dialect: 'postgres',
-      host: 'db',
-      models: [],
-      password: 'root',
-      port: 5432,
-      synchronize: true,
-      username: 'root',
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        autoLoadModels: config.get<boolean>('DB_AUTOLOAD_MODELS'),
+        database: config.get<string>('DB_DATABASENAME'),
+        dialect: config.get<Dialect>('DB_DIALECT'),
+        host: config.get<string>('DB_HOST'),
+        password: config.get<string>('DB_PASSWORD'),
+        port: config.get<number>('DB_PORT'),
+        synchronize: config.get<boolean>('DB_SYNCHRONIZE'),
+        username: config.get<string>('DB_USERNAME'),
+      }),
     }),
     ConfigModule.forRoot({
       isGlobal: true,
